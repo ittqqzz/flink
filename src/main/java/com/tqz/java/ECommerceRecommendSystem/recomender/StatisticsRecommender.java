a16045cc9ec1bcd6eaeab7b61c69c313575d9a33
tqz <572393128@qq.com>
2019-10-13 19:39:22 +0800

    离线推荐

---
+++ src/main/java/com/tqz/java/ECommerceRecommendSystem/recomender/StatisticsRecommender.java
@@ -0,0 +1,17 @@
+package com.tqz.java.ECommerceRecommendSystem.recomender;
+
+/**
+ * 离线推荐
+ * 离线推荐服务主要分为
+ * - 统计推荐、
+ * - 基于隐语义模型的协同过滤推荐
+ * - 以及基于内容和基于Item-CF的相似推荐
+ */
+public class StatisticsRecommender {
+
+    // 加载 mongodb 的数据
+
+    // 数据源转为 table
+
+    // 使用 flink 的 sql 执行统计，视频： 10/29
+}
